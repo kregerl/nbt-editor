a097459cfac247c5f91e8b3f10f97cf8e7c98441
kregerl <loucaskreger@gmail.com>
2024-05-18 10:54:17 -0400

    Added popup window with editor for floats

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@
 #![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
 use std::{
-    collections::BTreeMap,
+    collections::{BTreeMap, HashMap},
     fs::File,
     io::{Read, Seek, SeekFrom},
     path::Path,
 };
 
-use eframe::egui::{self, collapsing_header::CollapsingState, SelectableLabel, Ui};
+use eframe::egui::{self, Id, Ui};
 use egui_dock::{DockArea, DockState, TabViewer};
 use log::{debug, info};
 use nbt::tag::{NBTMap, NBTValue};
@@ -25,12 +25,13 @@ fn main() -> Result<(), eframe::Error> {
         "NBT Editor",
         options,
         // Box::new(|_cc| Box::new(NBTEditor::default())),
-        Box::new(|_cc| Box::new(NBTEditor::new("playerdata.dat").unwrap())),
+        Box::new(|_cc| Box::new(NBTEditor::new(Path::new("playerdata.dat")).unwrap())),
     )
 }
 
 #[derive(Default)]
 struct Tabs {
+    is_editor_window_open: HashMap<Id, bool>,
     buffers: BTreeMap<String, NBTMap>,
 }
 
@@ -38,7 +39,122 @@ impl Tabs {
     pub fn new(title: &str, contents: NBTMap) -> Self {
         let mut map = BTreeMap::new();
         map.insert(title.to_owned(), contents);
-        Self { buffers: map }
+        Self {
+            is_editor_window_open: HashMap::new(),
+            buffers: map,
+        }
+    }
+
+    fn push_nbt_map(&mut self, ui: &mut Ui, tab: &mut <Tabs as TabViewer>::Tab) {
+        let map = self.buffers.get_mut(tab).unwrap();
+        for (key, value) in &mut map.content {
+            Self::push_nbt_value(key, value, ui, &mut self.is_editor_window_open);
+        }
+    }
+
+    fn push_nbt_value(
+        name: &str,
+        tag: &mut NBTValue,
+        ui: &mut Ui,
+        is_editor_window_open: &mut HashMap<Id, bool>,
+    ) {
+        let label = if !name.is_empty() {
+            format!("{}: ", name)
+        } else {
+            "".into()
+        };
+
+        match tag {
+            NBTValue::Byte(n) => {
+                ui.label(format!("[B] {}{}", label, n));
+            }
+            NBTValue::Short(n) => {
+                ui.label(format!("[S] {}{}", label, n));
+            }
+            NBTValue::Int(n) => {
+                ui.label(format!("[I] {}{}", label, n));
+            }
+            NBTValue::Long(n) => {
+                ui.label(format!("[L] {}{}", label, n));
+            }
+            NBTValue::Float(n) => {
+                let id = ui.next_auto_id();
+                let text = format!("[F] {}{}", label, n);
+
+                ui.push_id(id, |ui| {
+                    if ui.selectable_label(false, &text).double_clicked() {
+                        debug!("Double clicked");
+                        is_editor_window_open.insert(id, true);
+                    }
+                });
+
+                if let Some(open_editor) = is_editor_window_open.get_mut(&id) {
+                    let mut contents = String::new();
+                    egui::Window::new(&text)
+                        .open(open_editor)
+                        .show(ui.ctx(), |ui| {
+                            ui.label(text);
+                            ui.text_edit_singleline(&mut contents);
+                        });
+                }
+            }
+            NBTValue::Double(n) => {
+                ui.label(format!("[D] {}{}", label, n));
+            }
+            NBTValue::String(n) => {
+                ui.label(n.clone());
+            }
+            NBTValue::List(list) => {
+                let len = list.len();
+                Self::push_collapsing(&label, std::iter::zip(vec![""; len], list), ui);
+            }
+            NBTValue::Compound(map) => {
+                let label = if label.is_empty() {
+                    format!("{} entries", map.len())
+                } else {
+                    label
+                };
+                Self::push_collapsing(
+                    &label,
+                    map.iter_mut().map(|(name, tag)| (name.as_str(), tag)),
+                    ui,
+                );
+            }
+            NBTValue::ByteArray(byte_array) => {
+                Self::push_array(byte_array, ui);
+            }
+            NBTValue::IntArray(int_array) => {
+                Self::push_array(int_array, ui);
+            }
+            NBTValue::LongArray(long_array) => {
+                Self::push_array(long_array, ui);
+            }
+        }
+    }
+
+    fn push_array<I: ToString>(array: &[I], ui: &mut Ui) {
+        ui.push_id(ui.next_auto_id(), |ui| {
+            for long in array {
+                ui.push_id(ui.next_auto_id(), |ui| {
+                    ui.label(long.to_string());
+                });
+            }
+        });
+    }
+
+    fn push_collapsing<'a, I>(label: &str, elements: I, ui: &mut Ui)
+    where
+        I: Iterator<Item = (&'a str, &'a mut NBTValue)>,
+    {
+        ui.push_id(ui.next_auto_id(), |ui| {
+            ui.collapsing(label, |ui| {
+                for (key, value) in elements {
+                    ui.push_id(ui.next_auto_id(), |ui| {
+                        Self::push_nbt_value(&key, value, ui, &mut HashMap::new());
+                    });
+                }
+            });
+        });
     }
 }
 
@@ -52,9 +168,8 @@ impl TabViewer for Tabs {
     fn ui(&mut self, ui: &mut Ui, tab: &mut Self::Tab) {
         ui.heading("NBT Editor");
         egui::ScrollArea::vertical().show(ui, |ui| {
-            let counter = 0;
-            let mut map = self.buffers.get_mut(tab).unwrap();
-            NBTEditor::push_nbt_map(&mut map, ui, counter);
+            // let mut map = self.buffers.get_mut(tab).unwrap();
+            self.push_nbt_map(ui, tab);
         });
     }
 }
@@ -77,10 +192,14 @@ impl Default for NBTEditor {
 const GZIP_SIGNATURE: [u8; 2] = [0x1f, 0x8b];
 const ZLIB_SIGNATURES: [[u8; 2]; 4] = [[0x78, 0x01], [0x78, 0x5e], [0x78, 0x9c], [0x78, 0xda]];
 impl NBTEditor {
-    pub fn new(file_path: &str) -> nbt::Result<Self> {
+    pub fn new(file_path: &Path) -> nbt::Result<Self> {
+        let file_name = match file_path.file_name() {
+            Some(s) => s.to_str().unwrap(),
+            None => "Untitled",
+        };
         Ok(Self {
-            tabs: Tabs::new("Testing", Self::read_nbt_file(Path::new(file_path))?),
-            state: DockState::new(vec!["Testing".to_owned()]),
+            tabs: Tabs::new(file_name, Self::read_nbt_file(file_path)?),
+            state: DockState::new(vec![file_name.to_owned()]),
         })
     }
 
@@ -119,151 +238,19 @@ impl NBTEditor {
         Ok(nbt_map)
     }
 
-    fn push_nbt_map(map: &mut NBTMap, ui: &mut Ui, counter: usize) {
-        for (key, value) in &mut map.content {
-            match value {
-                nbt::tag::NBTValue::ByteArray(_) => Self::push_nbt_value(key, value, ui, counter),
-                nbt::tag::NBTValue::List(_) => Self::push_nbt_value(key, value, ui, counter),
-                nbt::tag::NBTValue::Compound(_) => Self::push_nbt_value(key, value, ui, counter),
-                nbt::tag::NBTValue::IntArray(_) => Self::push_nbt_value(key, value, ui, counter),
-                nbt::tag::NBTValue::LongArray(_) => Self::push_nbt_value(key, value, ui, counter),
-                _ => Self::push_nbt_value(key, value, ui, counter),
-            };
-        }
-    }
-
-    fn push_nbt_value(name: &str, tag: &mut NBTValue, ui: &mut Ui, counter: usize) {
-        let label = if !name.is_empty() {
-            format!("{}: ", name)
-        } else {
-            "".into()
-        };
-
-        match tag {
-            NBTValue::Byte(n) => {
-                ui.label(format!("[B] {}{}", label, n));
-            }
-            NBTValue::Short(n) => {
-                ui.label(format!("[S] {}{}", label, n));
-            }
-            NBTValue::Int(n) => {
-                ui.label(format!("[I] {}{}", label, n));
-            }
-            NBTValue::Long(n) => {
-                ui.label(format!("[L] {}{}", label, n));
-            }
-            NBTValue::Float(n) => {
-                if ui
-                    .selectable_label(false, format!("[F] {}{}", label, n))
-                    .double_clicked()
-                {
-                    debug!("Double clicked");
-                    *n += 1.0;
-                }
-                // ui.label(format!("[F] {}{}", label, n));
-            }
-            NBTValue::Double(n) => {
-                ui.label(format!("[D] {}{}", label, n));
-            }
-            NBTValue::String(n) => {
-                ui.label(n.clone());
-            }
-            NBTValue::List(list) => {
-                let len = list.len();
-                Self::push_collapsing(&label, std::iter::zip(vec![""; len], list), counter, ui);
-            }
-            NBTValue::Compound(map) => {
-                let label = if label.is_empty() {
-                    format!("{} entries", map.len())
-                } else {
-                    label
-                };
-                Self::push_collapsing(
-                    &label,
-                    map.iter_mut().map(|(name, tag)| (name.as_str(), tag)),
-                    counter,
-                    ui,
-                );
-            }
-            NBTValue::ByteArray(byte_array) => {
-                Self::push_array(byte_array, ui, counter);
-            }
-            NBTValue::IntArray(int_array) => {
-                Self::push_array(int_array, ui, counter);
-            }
-            NBTValue::LongArray(long_array) => {
-                Self::push_array(long_array, ui, counter);
-            }
-        }
-    }
-
-    fn push_array<I: ToString>(array: &[I], ui: &mut Ui, mut counter: usize) {
-        counter += 1;
-        ui.push_id(counter, |ui| {
-            for long in array {
-                counter += 1;
-                ui.push_id(counter, |ui| {
-                    ui.label(long.to_string());
-                });
-            }
-        });
-    }
-
-    fn push_collapsing<'a, I>(label: &str, elements: I, mut counter: usize, ui: &mut Ui)
-    where
-        I: Iterator<Item = (&'a str, &'a mut NBTValue)>,
-    {
-        counter += 1;
-        ui.push_id(counter, |ui| {
-            ui.collapsing(label, |ui| {
-                for (key, value) in elements {
-                    counter += 1;
-                    ui.push_id(counter, |ui| {
-                        Self::push_nbt_value(&key, value, ui, counter);
-                    });
-                }
-            });
-        });
-    }
-
-    fn update_side_panel(&mut self, ctx: &egui::Context) {
-        // Side panel hierarchy for open projects & nbt tags
-        egui::SidePanel::left("heirarchy").show(ctx, |ui| {
-            ui.heading("NBT Editor");
-            egui::ScrollArea::vertical().show(ui, |ui| {
-                let mut counter = 0;
-                for (key, value) in &mut self.tabs.buffers {
-                    counter += 1;
-                    CollapsingState::load_with_default_open(
-                        ctx,
-                        ui.make_persistent_id(counter),
-                        true,
-                    )
-                    .show_header(ui, |ui| ui.label(format!("{} [root]", key)))
-                    .body(|ui| {
-                        ui.push_id(counter, |ui| {
-                            NBTEditor::push_nbt_map(value, ui, counter);
-                        });
-                    });
-                }
-            });
-        });
-    }
-
     fn update_central_panel(&mut self, ctx: &egui::Context) {
         // Tabbed central panel for editing and viewing nbt files
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("LABEL");
-            // for title in self.tabs.buffers.keys() {
-            //     let tab_location = self.state.find_tab(title);
-            //     if ui.selectable_label(tab_location.is_some(), title).clicked() {
-            //         if let Some(tab_location) = tab_location {
-            //             self.state.set_active_tab(tab_location);
-            //         } else {
-            //             self.state.push_to_focused_leaf(title.clone());
-            //         }
-            //     }
-            // }
+            for title in self.tabs.buffers.keys() {
+                let tab_location = self.state.find_tab(title);
+                if ui.selectable_label(tab_location.is_some(), title).clicked() {
+                    if let Some(tab_location) = tab_location {
+                        self.state.set_active_tab(tab_location);
+                    } else {
+                        self.state.push_to_focused_leaf(title.clone());
+                    }
+                }
+            }
         });
     }
 
@@ -296,7 +283,7 @@ impl eframe::App for NBTEditor {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         self.update_menu_bar(ctx);
 
-        self.update_side_panel(ctx);
+        // self.update_side_panel(ctx);
 
         if self.tabs.buffers.is_empty() {
             egui::CentralPanel::default().show(ctx, |ui| {
